24d997be26229768af22eacc5ed48e0bdcb23736
Jonathan <navakino@gmail.com>
2022-10-13 11:42:31 -0500

    Create the related items component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
+import RelatedItems from './RelatedItems.jsx';
 
 const App = () =>{
-    return (
-        <h1>
-            Welcome to React App thats build using Webpack and Babel separately
-        </h1>
-    )
-}
+  return (
+    <>
+      <h1>
+        Welcome to React App thats build using Webpack and Babel separately
+      </h1>
+      <RelatedItems />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ client/components/RelatedItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RelatedItems = () => {
+  return (
+    <div>
+      This is the related items component.
+    </div>
+  );
+};
+
+export default RelatedItems;
