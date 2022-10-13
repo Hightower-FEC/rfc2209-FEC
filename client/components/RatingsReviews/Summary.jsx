098634fbc0b50306958fa8881969d9e5ba79d2b1
Miles Owens <milesrhowens@gmail.com>
2022-10-13 14:41:24 -0700

    Initialize Summary.jsx boiler-plate code

---
+++ client/components/RatingsReviews/Summary.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+
+const Summary = () =>{
+  return (
+    <div>
+      <h1>
+      Summary
+      </h1>
+      <div>
+      </div>
+    </div>
+
+  );
+};
+
+export default Summary;
