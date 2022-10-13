bd9b895c3892d0b73c93051611b8e91e2950d68b
Miles Owens <milesrhowens@gmail.com>
2022-10-13 12:40:50 -0700

    Removed old config folder

--- client/config/config.js
+++
@@ -1,6 +0,0 @@
-// Your API key goes here!
-var API_KEY = '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';
-var CAMPUS_CODE = 'hr-rfc';
-var BASE_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/';
-
-export { API_KEY, BASE_URL, CAMPUS_CODE };
--- client/config/example.config.js
+++
@@ -1,7 +0,0 @@
-// Your API key goes here!
-var API_KEY = 'GIT KEY GOES HERE';
-var CAMPUS_CODE = 'CAMPUS CODE GOES HERE';
-
-var URL = `https://app-hrsei-api.herokuapp.com/api/fec2/${CAMPUS_CODE}`;
-
-export { API_KEY, URL };
