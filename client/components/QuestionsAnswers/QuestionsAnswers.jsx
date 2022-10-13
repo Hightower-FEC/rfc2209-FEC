7f8c8352c481c8eaf9f8c788d1bc05b610be3097
Tuan Nguyen <tnguyen4224@gmail.com>
2022-10-13 16:56:20 -0500

    Render static search component

--- client/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ client/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
 
-const QuestionsAnswers = () =>{
-    return (
-        <h1>
-            QuestionsAnswers
-        </h1>
-    )
-}
+const QuestionsAnswers = ({productID}) => {
+  // productID = 66642
 
-export default QuestionsAnswers
+
+  const getQuestions = () => {
+
+  };
+  const getAnswers = () => {
+
+  };
+
+  return (
+    <div>
+      <h1>Questions and Answers</h1>
+      <Search />
+    </div>
+  );
+};
+
+const Search = () => {
+  // Set search state to an empty string
+  const [search, setSearch] = useState('');
+
+  const handleSearch = ((e) => {
+    e.preventDefault();
+    console.log('Submit search for: ', search);
+    setSearch('');
+  });
+
+  return (
+    <form onSubmit={handleSearch}>
+      <input id='search' onChange={((e) => setSearch(e.target.value))} value = {search} placeholder ='Have a question? Search for answers...' />
+      <button type='submit'>🔍︎</button>
+    </form>
+  );
+};
+
+const QuestionList = ({}) => {
+
+};
+
+
+export default QuestionsAnswers;
