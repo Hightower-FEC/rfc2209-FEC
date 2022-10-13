ba0664719eda3d7376287127b1a2c893597fe174
Miles Owens <milesrhowens@gmail.com>
2022-10-13 14:41:03 -0700

    Initialize Reviews.jsx boiler-plate

---
+++ client/components/RatingsReviews/Reviews.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+
+const Reviews = () =>{
+  return (
+    <div>
+      <h1>
+      Reviews
+      </h1>
+      <div>
+      </div>
+    </div>
+
+  );
+};
+
+export default Reviews;
