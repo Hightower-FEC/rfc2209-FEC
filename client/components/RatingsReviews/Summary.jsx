ff2e08ceb6c19b5621be1d082a53ef73ca55ff80
Miles Owens <milesrhowens@gmail.com>
2022-10-14 14:12:28 -0700

    Added boiler plate to change with real data later

--- client/components/RatingsReviews/Summary.jsx
+++ client/components/RatingsReviews/Summary.jsx
@@ -1,12 +1,50 @@
 import React, {useState, useEffect} from 'react';
 
-const Summary = () =>{
+import Stars from '../Stars.jsx';
+
+const Summary = ({productID}) =>{
   return (
     <div>
-      <h1>
-      Summary
+      <h1 style={{display: 'flex', flexDirection: 'row'}}>
+        3.5 <Stars productID={productID}/>
       </h1>
       <div>
+        <div>
+          100% of reviews recommend this product
+        </div>
+
+        <div>
+          <div>
+            <a>5 stars</a><a>-----</a>
+          </div>
+          <div>
+            <a>4 stars</a><a>-----</a>
+          </div>
+          <div>
+            <a>3 stars</a><a>-----</a>
+          </div>
+          <div>
+            <a>2 stars</a><a>-----</a>
+          </div>
+          <div>
+            <a>1 stars</a><a>-----</a>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <div>
+              <a>Size</a>
+            </div>
+            <a>-----------</a>
+          </div>
+          <div>
+            <div>
+              <a>Comfort</a>
+            </div>
+            <a>-----------</a>
+          </div>
+        </div>
       </div>
     </div>
 
