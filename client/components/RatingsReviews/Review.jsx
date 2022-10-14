a1154caa6608a856a456ae1c73153103d4bb8974
Miles Owens <milesrhowens@gmail.com>
2022-10-14 14:13:43 -0700

    Init React component

---
+++ client/components/RatingsReviews/Review.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+
+const Review = ({productID}) =>{
+  return (
+    <div>
+      <h1>
+        Review
+      </h1>
+      <div>
+      </div>
+    </div>
+
+  );
+};
+
+export default Review;
