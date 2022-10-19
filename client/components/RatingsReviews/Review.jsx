0dceca240cdebe6865ee9d72e352159ce54ed233
Miles Owens <milesrhowens@gmail.com>
2022-10-18 21:53:01 -0700

    Review dynamically displays contents of a review

--- client/components/RatingsReviews/Review.jsx
+++ client/components/RatingsReviews/Review.jsx
@@ -1,12 +1,21 @@
 import React, {useState, useEffect} from 'react';
 
-const Review = ({review}) =>{
+import Stars from '../Stars.jsx';
+const Review = ({productID, review}) =>{
   return (
     <div>
-      <h1>
-        {review.review_id}
-      </h1>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        <Stars productID={productID}/>
+        <a>{review.reviewer_name}, {review.date}</a>
+      </div>
+      <h4>
+        {review.body}
+      </h4>
       <div>
+        <p>{review.summary}</p>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        Helpful? Yes ({review.helpfulness}) | Report
       </div>
     </div>
 
