850deeb68042bf74d7cf69da5fdfbf40c773dd8a
Miles Owens <milesrhowens@gmail.com>
2022-10-19 14:19:34 -0700

    Changed svg to give stars a border

--- client/components/Star.jsx
+++ client/components/Star.jsx
@@ -2,13 +2,29 @@ import React from 'react';
 
 const Star = ()=>{
   return (
-    <svg id="eh8DsINBfnY1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" shapeRendering="geometricPrecision" textRendering="geometricPrecision" style={{backgroundColor: 'transparent'}}>
-      <polygon points="0,-8.776276 8.346734,-2.712018 5.158565,7.100156 -5.158565,7.100156 -8.346734,-2.712018 0,-8.776276" transform="translate(4.00644 1.981631)" strokeWidth="0" fill="white"/>
-      <polygon points="0,-7.651491 7.277001,-2.364441 4.497434,6.190187 -4.497434,6.190187 -7.277001,-2.364441 0,-7.651491" transform="matrix(1.1 0 0 1.1 19.79293 2.21283)" strokeWidth="0" fill="white"/>
-      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="translate(12 28.729699)" strokeWidth="0" fill="white"/>
-      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="matrix(1.017059 0 0 1.025565 27.202182 17.86805)" strokeWidth="0" fill="white"/>
-      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="matrix(1.017059 0 0 1.025565-3.200588 17.86805)" strokeWidth="0" fill="white"/>
+    <svg id="eHPDQqGymj41" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" shapeTendering="geometricPrecision" textTendering="geometricPrecision">
+      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="translate(26.6 17.609597)" strokeWidth="0"/>
+
+      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="translate(20.81441 0.6)" strokeWidth="0"/>
+
+      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="translate(-2.5 17.65)" strokeWidth="0"/>
+
+      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="translate(12 28.25)" strokeWidth="0"/>
+
+      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="translate(12 28.729699)" fill="#fff" strokeWidth="0"/><polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="matrix(1.017059 0 0 1.025565 27.202182 17.86805)" fill="#fff" strokeWidth="0"/>
+
+      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="translate(3.090448 0.6)" strokeWidth="0"/>
+
+      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="matrix(1.017059 0 0 1.025565-3.200588 17.86805)" fill="#fff" strokeWidth="0"/>
+
+      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="matrix(1.017059 0 0 1.025565 21.284288-.045271)" fill="#fff" strokeWidth="0"/>
+
+      <polygon points="0,-11.000438 10.462038,-3.399322 6.465895,8.899541 -6.465895,8.899541 -10.462038,-3.399322 0,-11.000438" transform="matrix(1.017059 0 0 1.025565 2.620571-.045271)" fill="#fff" strokeWidth="0"/>
+
+      <polygon points="0,-15.166059 14.42378,-4.040836 8.914386,13.960154 -8.914386,13.960154 -14.42378,-4.040836 0,-15.166059" transform="matrix(0 0 0 0 0 0)" fill="#d2dbed" strokeWidth="0"/>
+
     </svg>
+
   );
 };
 
