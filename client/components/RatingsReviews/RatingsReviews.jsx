2b1803fd22d898def9f944d3554c643d7b18eb23
Miles Owens <milesrhowens@gmail.com>
2022-10-18 21:40:35 -0700

    Add a sortBy func that is passed to Reviews component

--- client/components/RatingsReviews/RatingsReviews.jsx
+++ client/components/RatingsReviews/RatingsReviews.jsx
@@ -7,29 +7,54 @@ import Reviews from './Reviews.jsx';
 import { URL } from '../../../config/config.js';
 
 const RatingsReviews = ({productID}) =>{
+  /**
+   * Init reviews as undefined - nothing is rendered unless this state has value
+   */
+  const [reviews, setReviews] = useState();
 
+  /**
+   * On render, try and get reviews using the productID
+   */
   useEffect(() => {
     axios.get(`${URL}/reviews?product_id=${productID}`)
       .then((response) => {
         console.log(response.data);
+        setReviews(response.data.results);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  return (
+  /**
+   * Retrieve reviews from API passing a specified sort parameter
+   */
+  const sortReviews = (sortBy) => {
+    axios.get(`${URL}/reviews?product_id=${productID}&sort=${sortBy}`)
+      .then((response) => {
+        console.log(response.data);
+        setReviews(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+
+  /**
+   * Only render Ratings and Reviews once we get reviews to render
+   */
+  return reviews ? (
     <div>
       <h3>
         RatingsReviews
       </h3>
       <div style={{display: 'flex', flexDirection: 'row'}}>
-        <Summary productID={productID}/>
-        <Reviews productID={productID}/>
+        <Summary productID={productID} reviews={reviews}/>
+        <Reviews productID={productID} reviews={reviews} handleSetSort={sortReviews}/>
       </div>
     </div>
-
-  );
+  ) : <></>;
 };
 
 export default RatingsReviews;
