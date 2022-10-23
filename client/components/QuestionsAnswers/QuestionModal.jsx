ad55d382779dcb3ae608186eb24a50fe637c89cf
Tuan Nguyen <tnguyen4224@gmail.com>
2022-10-23 15:53:54 -0500

    Remove alert from question modal and add red text error messages

--- client/components/QuestionsAnswers/QuestionModal.jsx
+++ client/components/QuestionsAnswers/QuestionModal.jsx
@@ -14,42 +14,100 @@ const QuestionModal = ({showQModal, onClose, name, submitQuestion}) => {
     top: '0',
     right: '0',
     bottom: '0',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    zIndex: '9999999'
   };
   const modalContent = {
-    width: '80%',
+    width: '60%',
+    height: '35%',
+    borderRadius: '1rem',
     backgroundColor: '#fff'
   };
-  const modalHeadFoot = {
-    padding: '10px'
+
+  const header = {
+    fontSize: '28px'
   };
+  const error = {
+    fontSize: '18px',
+    color: 'red'
+  };
+
   const modalBody = {
     padding: '10px',
-    borerTop: '1px solid #eee',
+    borderTop: '1px solid #eee',
     borderBottom: '1px solid #eee',
+  };
+
+  const footer = {
+    display: 'flex',
+    justifyContent: 'center'
+  };
+  const btn = {
+    display: 'inline-block',
+    margin: '10px 4rem 0 0',
+    justifyContent: 'center',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: '18px',
+    width: '20%',
+    height: '60px'
   };
 
   // Helper function to verify email address
   const verifyEmail = (email) => {
-    let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
-    return regex.test(email);
+    let checkEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return email.match(checkEmail);
   };
-
+  // Helper function to create a span that displays the error message on submission
+  const createErrorMsg = (message) => {
+    let errorSpan = document.createElement('span');
+    errorSpan.innerHTML = `<span>${message}</span><br/>`;
+    return errorSpan;
+  };
   // Pass formatted question data to parent component
   const handleSubmit = () => {
-    if (ask === '' || nickname === '' || email === '' || !verifyEmail(email)) {
-      alert('You must enter the correct information');
-    } else {
+    // Flags for checking if input is correct
+    let valid = true;
+
+    // Remove any error messsage that appeared from previous submission
+    let errors = document.getElementsByClassName('error')[0];
+    while (errors.firstChild) {
+      errors.removeChild(errors.firstChild);
+    }
+
+    // Check input fields
+    if (ask === '') { // Questin field
+      valid = false;
+      let error = createErrorMsg('Question cannot be blank');
+      document.getElementsByClassName('error')[0].appendChild(error);
+    }
+    if (nickname === '') { // Nickname field
+      valid = false;
+      let error = createErrorMsg('Nickname cannot be blank');
+      document.getElementsByClassName('error')[0].appendChild(error);
+    }
+    if (email === '') { // Email field
+      valid = false;
+      let error = createErrorMsg('Email cannot be blank');
+      document.getElementsByClassName('error')[0].appendChild(error);
+    } else if (!verifyEmail(email)) {
+      valid = false;
+      let error = createErrorMsg('Email is not valid');
+      document.getElementsByClassName('error')[0].appendChild(error);
+    }
+
+    // If still valid, then submit form
+    if (valid) {
       let formatQuestion = {
         question_body: ask,
         asker_name: nickname,
-        asker_email: email,
-        question_helpfulness: 0,
-        answers: {},
-        reported: false
+        asker_email: email
+        // question_helpfulness: 0,
+        // answers: {},
+        // reported: false
       };
       submitQuestion(formatQuestion);
       onClose();
@@ -60,9 +118,12 @@ const QuestionModal = ({showQModal, onClose, name, submitQuestion}) => {
     <div className='modal' style={modalStyle} onClick={onClose}>
       <div className='modal-content' style={modalContent} onClick={(e) => e.stopPropagation()}>
         {/* Modal Header */}
-        <div className='modal-header' style={modalHeadFoot}>
-          <h4 className='modal-timer'>Ask Your Question</h4>
-          <h5>About the {name}</h5>
+        <div className='modal-header' style={header}>
+          <h4>Ask Your Question</h4>
+          <h4 className='product'>About the {name}</h4>
+          <div className='error' style={error}></div><br/>
+          {/* <span className='error2' style={error}></span><br/>
+          <span className='error3' style={error}></span> */}
         </div>
 
         {/* Modal Body */}
@@ -79,9 +140,9 @@ const QuestionModal = ({showQModal, onClose, name, submitQuestion}) => {
         </div>
 
         {/* Modal Footer */}
-        <div className='modal-footer' style={modalHeadFoot}>
-          <button className='submitBtn' onClick={handleSubmit}>Submit</button>
-          <button className='closeBtn' onClick={onClose}>Close</button>
+        <div className='modal-footer' style={footer}>
+          <button className='submitBtn' onClick={handleSubmit}style={btn}>Submit</button>
+          <button className='closeBtn' onClick={onClose} style={btn} >Close</button>
         </div>
       </div>
     </div>
