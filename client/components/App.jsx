e0f90d4fc4cd20cf6861f1a362553b4e72b90aa3
Miles Owens <milesrhowens@gmail.com>
2022-10-23 20:09:04 -0700

    Accidentally committed experimental changes - changes undone

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -25,14 +25,6 @@ const App = () => {
       .catch((error) => {
         console.log(error);
       });
-    axios.put('/reviews/2/report')
-      .then((response) => {
-        console.log('hi');
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      });
   }, []);
 
   const handleRelatedItemClick = (id) => {
@@ -42,11 +34,11 @@ const App = () => {
   if (currentProductID) {
     return (
       <div>
-        {/* <TopBar/> */}
-        {/* <Overview productID={currentProductID}/> */}
-        {/* <RelatedItems productID={currentProductID} handleRelatedItemClick={handleRelatedItemClick}/> */}
-        {/* <QuestionsAnswers productID={currentProductID}/>
-        <RatingsReviews productID={currentProductID}/> */}
+        <TopBar/>
+        <Overview productID={currentProductID}/>
+        <RelatedItems productID={currentProductID} handleRelatedItemClick={handleRelatedItemClick}/>
+        <QuestionsAnswers productID={currentProductID}/>
+        <RatingsReviews productID={currentProductID}/>
       </div>
     );
   }
