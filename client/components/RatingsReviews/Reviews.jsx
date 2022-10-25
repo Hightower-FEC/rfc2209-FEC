cef5dedd500f020d43e9461e20a21eb75ccf61c0
Miles Owens <milesrhowens@gmail.com>
2022-10-25 09:21:46 -0700

    Refactor for not being passed reviews as prop

--- client/components/RatingsReviews/Reviews.jsx
+++ client/components/RatingsReviews/Reviews.jsx
@@ -2,17 +2,36 @@ import React, {useState, useEffect} from 'react';
 import Review from './Review.jsx';
 import axios from 'axios';
 
-const Reviews = ({productID, reviews, handleSetSort}) =>{
+import PostReviewModal from './PostReviewModal.jsx';
+
+const Reviews = ({productID, handleSetSort}) =>{
+  /**
+   * Init reviews as undefined - nothing is rendered unless this state has value
+   */
+  const [reviews, setReviews] = useState();
   const [sortedBy, setSortedBy] = useState('relevance');
   const [numofReviewsToRender, setNumofReviewsToRender] = useState(2);
   const sorts = ['relevance', 'helpfulness', 'newest'];
+  const [showModal, setShowModal] = useState(false);
 
+  /**
+   * On render, try and get reviews using the productID
+   */
   useEffect(() => {
-    handleSetSort(sortedBy);
-  }, [sortedBy]);
-
-  const renderedReviews = reviews.slice(0, numofReviewsToRender);
-
+    axios.get(`reviews?product_id=${productID}`)
+      .then((response) => {
+        axios.get(`reviews?product_id=${productID}&sort=${sortBy}`)
+          .then((response) => {
+            setReviews(response.data.results);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [productID]);
 
   /**
    * Increments number of reviews to render
@@ -28,13 +47,20 @@ const Reviews = ({productID, reviews, handleSetSort}) =>{
     console.log('Add a review clicked');
   };
 
+  const handleCloseModal = () => {
+    console.log('Later');
+  };
+
+  const renderedReviews = reviews ? reviews.slice(0, numofReviewsToRender) : null;
+
+
   /**
    * Handles whether More Reviews button is rendered or not
    * So long as there are more reviews to render, then it will render
    */
-  const moreReviews = renderedReviews.length < reviews.length ? <button onClick={handleMoreReviewsClick}>More Reviews</button> : null;
+  const moreReviews = renderedReviews && renderedReviews.length < reviews.length ? <button onClick={handleMoreReviewsClick}>More Reviews</button> : null;
 
-  return (
+  return renderedReviews ? (
     <div>
       {/* Header */}
       <h4>
@@ -63,9 +89,11 @@ const Reviews = ({productID, reviews, handleSetSort}) =>{
         {moreReviews}
         <button onClick={handleAddReviewClick}>Add a review +</button>
       </div>
+
+      <PostReviewModal/>
     </div>
 
-  );
+  ) : <></>;
 };
 
 export default Reviews;
