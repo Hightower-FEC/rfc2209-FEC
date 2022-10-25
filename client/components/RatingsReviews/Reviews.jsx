519a595505d2546e65d1130f5448cd3938475717
LikeMike07 <mraisc07@gmail.com>
2022-10-25 16:26:30 -0600

    added styles to ratings and stars component

--- client/components/RatingsReviews/Reviews.jsx
+++ client/components/RatingsReviews/Reviews.jsx
@@ -65,7 +65,7 @@ const Reviews = ({productID, handleSetSort}) =>{
   const moreReviews = renderedReviews && renderedReviews.length < reviews.length ? <button onClick={handleMoreReviewsClick}>More Reviews</button> : null;
 
   return renderedReviews ? (
-    <div>
+    <div className="reviews-container">
       {/* Header */}
       <h4>
         {reviews.length} reviews, sorted by
