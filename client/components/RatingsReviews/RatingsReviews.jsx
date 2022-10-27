5b2cccf6189ad32abf5124ec615b11eaed11b687
Miles Owens <milesrhowens@gmail.com>
2022-10-26 21:15:19 -0700

    Problem with merge - no changes

--- client/components/RatingsReviews/RatingsReviews.jsx
+++ client/components/RatingsReviews/RatingsReviews.jsx
@@ -13,14 +13,41 @@ const RatingsReviews = ({productID}) =>{
   const [reviews, setReviews] = useState();
 
   /**
+   * On render, try and get reviews using the productID
+   */
+  useEffect(() => {
+    axios.get(`reviews?product_id=${productID}`)
+      .then((response) => {
+        setReviews(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [productID]);
+
+  /**
+   * Retrieve reviews from API passing a specified sort parameter
+   */
+  const sortReviews = (sortBy) => {
+    axios.get(`reviews?product_id=${productID}&sort=${sortBy}`)
+      .then((response) => {
+        setReviews(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+
+  /**
    * Only render Ratings and Reviews once we get reviews to render
    */
   return reviews ? (
-    <div>
-      <h3>
+    <div style={{margin: '0 10rem 0 10rem'}}>
+      <h1 style={{margin: '40px 0 10px 0'}}>
         RatingsReviews
-      </h3>
-      <div style={{display: 'flex', flexDirection: 'row'}}>
+      </h1>
+      <div className="ratings-reviews">
         <Summary productID={productID} reviews={reviews}/>
         <Reviews productID={productID} reviews={reviews} handleSetSort={sortReviews}/>
       </div>
