0ab4a9d1beb41f4dec9c567b8e030c66a6cca093
Tuan Nguyen <tnguyen4224@gmail.com>
2022-10-28 12:38:58 -0500

    Implement image pop up for reviews

--- client/components/RatingsReviews/Review.jsx
+++ client/components/RatingsReviews/Review.jsx
@@ -135,7 +135,50 @@ const Photo = ({url}) => {
   return (
     <>
       <div style={imageStyle} onClick={() => growImage(true)}></div>
+
+      <ImageModal
+        url={url}
+        showImage={showImage}
+        onClose={() => growImage(false)}
+      />
     </>
+  );
+};
+
+const ImageModal = ({url, showImage, onClose}) => {
+
+  const modal = {
+    position: 'fixed',
+    left: '0',
+    top: '0',
+    right: '0',
+    bottom: '0',
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: '999'
+  };
+  const modalContent = {
+    textAlign: 'center'
+  };
+  const modalImage = {
+    display: 'block',
+    justifyContent: 'center',
+    border: '10px solid white',
+    width: '100%',
+    height: '100%',
+    maxWidth: '75rem',
+    maxHeight: '75em'
+  };
+
+  return (
+    (showImage) &&
+    (<div className="img-modal" onClick={onClose} style={modal}>
+      <div className="modal-body" style={modalContent}>
+        <img src={url} id="modal-image" style={modalImage}/>
+      </div>
+    </div>)
   );
 };
 
